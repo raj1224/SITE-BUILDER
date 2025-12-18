d6656e7e7b2e070218bf81a11bc9e56f5c8fb2ae
Raj Kumar <imraaj1026@gmail.com>
2025-12-18 18:42:42 +0530

    myprojects ui creating

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import type {Project}  from '../types';
+import { Loader2Icon, PlusIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const MyProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const navigate = useNavigate()
+
+  const fetchProjects=async()=>{
+    // Simulate Loading
+    setTimeout(()=>{
+      setLoading(false)
+    })
+  }
+  useEffect(()=>{
+    fetchProjects()
+  },[])
+
+
   return (
-    <div>
-        <h1>MyProjects</h1>
+   <>
+    <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
+      {loading ? (
+        <div className='flex items-center justify-center h-[80vh]'>
+          <Loader2Icon className='size-7 animate-spin text-indigo-200'/>
+        </div>
+      ):projects.length > 0 ?(
+        <div className='py-10 min-h-[80vh]'>
+          <div className='flex items-center justify-between mb-12'>
+            <h1 className='text-2xl font-medium text-white'>My Projects</h1>
+            <button onClick={()=>navigate('/')} className='flex items-center gap-2 text-white px-3 sm:px-6 py-1 sm:py-2 rounded bglinear-to-br from-indigo-500 to-indigo-600 hover:opacity-90 active:scale-95 transition-all'>
+              <PlusIcon size={18}/> Create New
+            </button>
+          </div>
+
+          <div></div>
+
+        </div>
+      ):(
+        <div className='flex flex-col items-center justify-center h-[80vh]'>
+          <h1 className='text-3xl font-semibold text-gray-300'>You have no projects yet!</h1>
+          <button onClick={()=>Navigate('/')} className='text-white px-5 py-2 mt-5 rounded-md bg-indigo-500 hover:bg-indigo-600 active:scale-95 transition-all'>
+            Create New
+          </button>
+        </div>
+      )}
     </div>
+   </>
   )
-}
+
 
 export default MyProjects
