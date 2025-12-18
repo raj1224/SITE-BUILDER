a1a71cb9c742830f27c9b82d5b338d1843943485
Raj Kumar <imraaj1026@gmail.com>
2025-12-18 19:11:46 +0530

    ui is building of myprojects section

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -35,7 +35,34 @@ const MyProjects = () => {
             </button>
           </div>
 
-          <div></div>
+          <div className='flex flex-wrap gap-3.5'>
+            {projects.map((project)=>(
+              <div key={project.id} className='relative group w-72 max-sm:mx-auto cursor-pointer bg-gray-900/60 border border-gray-700 rounded-lg overflow-hidden shadow-md group hover:shadow-indigo-700/30 hover:border-indigo-800/80 transition-all duration-300'>
+                <div className='relative w-full h-40 bg-gray-900 overflow-hidden border-b border-gray-800'>
+                  {project.current_code ? (
+                    <iframe
+                    srcDoc={project.current_code}
+                    className='absolute top-0 left-0 w-[1200px] h-[800px] origin-top-left pointer-events-none 'sandbox='allow-scripts allow-same-origin'
+                    style={{transform:'scale(0.25'}}/>
+                  ):
+                  (
+                    <div className='flex items-center justify-center h-full text-gray-500'>
+                      <p>No Preview</p>
+                    </div>
+                  )}
+                </div>
+                {/* Content  */}
+                <div className='p-4 text-white bg-linear-180 from-transparent group-hover:from-indigo-950 to-transparent transition-colors '>
+                  <div className='flex items-start justify-between'>
+                    <h2 className='text-lg font-medium line-clamp-2'>{project.name}</h2>
+                    <button className='px-2.5 py-0.5 mt-1 ml-2 text-xs bg-gray-800 border border-gray-700 rounded-full'>Website</button>
+
+                  </div>
+                  <p>{project.initial_prompt}</p>
+                </div>
+              </div>
+            ))}
+          </div>
 
         </div>
       ):(
