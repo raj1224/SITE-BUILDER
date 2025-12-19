b151aa1ea0cb7915f6bf35efc74e51159261fd6d
Raj Kumar <imraaj1026@gmail.com>
2025-12-19 12:25:58 +0530

    building projects ui

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import { Laptop2Icon, Loader2Icon, MessageSquare, MessageSquareIcon, TabletIcon, XIcon } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyConversations, dummyProjects } from '../assets/assets'
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
+  const {projectId}=useParams()
+  const navigate = useNavigate()
+
+  const [project,setProject]=useState<Project | null>(null)
+  const [loading,setLoading]=useState(true)
+
+  const [isGenerating,setIsGenerating]= useState(true)
+  const[device,setDevice]=useState<'phone' | 'tablet' | 'desktop'>("desktop")
+
+  const [isMenuOpen,setIsMenuOpen]=useState(false)
+  const [isSaving,setIsSaving]=useState(false)
+
+  const fetchProject= async()=>{
+    const project= dummyProjects.find(project=>project.id===projectId)
+    setTimeout(() => {
+      if(project){
+        setProject({...project,conversation:dummyConversations});
+        setLoading(false)
+        setIsGenerating(project.current_code? false:true)
+      }
+    }, 2000);
+
+  }
+  useEffect(()=>{
+    fetchProject()
+  },[])
+  if(loading){
+    return(
+      <>
+      <div className='flex items-center justify-center h-screen '>
+        <Loader2Icon className='size-7 animate-spin text-violet-200'/>
+
+      </div>
+      </>
+    )
+  }
+
+
+  return project ? (
+    <div className='flex flex-col h-screen w-full bg-gray-900 text-white'>
+      {/* builder navbar  */}
+      <div className='flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar'>
+        {/* left  */}
+        <div className='flex items-center gap-2 sm:min-w-90 text-nowrap'>
+          <img src="/favicon.svg" alt="logo" className='h-6 cursor-pointer' onClick={()=>navigate('/')} />
+          <div className='max-w-64 sm:max-w-xs'>
+            <p className='text-sm text-medium capitalize truncate'>{project.name}</p>
+            <p className='text-xs text-gray-400 -mt-0.5'>Previewing last saved version</p>
+
+          </div>
+          <div className='sm:hidden flex-1 flex justify-end'>
+            {isMenuOpen ? 
+            <MessageSquareIcon onClick={()=>setIsMenuOpen(false)} className='size-6 cursor-pointer'/>
+          : <XIcon onClick={()=>setIsMenuOpen(true)} className='size-6 cursor-pointer'/>}
+          </div>
+        </div>
+        {/* middle  */}
+        <div>
+          <SmartphoneIcon onClick={()=>setDevice('phone')} className={`size-6 p-1 rounded cursor-pointer ${device==='phone' ? 'bg-gray-700' : ""}`} />
+
+          <TabletIcon onClick={()=>setDevice('tablet')} className={`size-6 p-1 rounded cursor-pointer ${device==='tablet' ? 'bg-gray-700' : ""}`} />
+
+          <LaptopIcon onClick={()=>setDevice('desktop')} className={`size-6 p-1 rounded cursor-pointer ${device==='desktop' ? 'bg-gray-700' : ""}`} />
+
+        </div>
+
+        {/* right  */}
+        
+
+      </div>
+      
+    </div>
   )
 }
 
